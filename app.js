a4f1824f28d15e68567909528760abc053a1dd2c
BorisCSanto <contact@chrismantcreation.fr>
2022-03-29 20:35:29 +0200

    app user ok

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const path = require('path');
+
+const authRoutes = require('./routes/auth');
+//const saucesRoutes = require('./routes/sauces');
+
+const app = express();
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static('images'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+//app.use('/api/sauces', saucesRoutes);
+
+
+module.exports = app
